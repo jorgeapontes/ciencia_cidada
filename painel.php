9d9060e21c444df043344106637af30c1752bc15
joao.alencar@hrjourney.com.br <joao.alencar@hrjourney.com.br>
2025-05-12 14:49:30 -0300

    enviando

--- painel.php
+++ painel.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['usuario_id'])) {
     <meta charset="UTF-8">
     <title>Painel - Aves Brasil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="painel.css">
 </head>
 <body class="bg-light">
 
