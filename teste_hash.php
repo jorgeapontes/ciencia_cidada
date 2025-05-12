fc193bfbc8634a6cf542e4530e6a60e4f17be6ef
Jorge <jorgeappontes13@gmail.com>
2025-05-12 15:21:42 -0300

    Corrigindo bug de admin

--- teste_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$senha = '12345';
-$hash = password_hash($senha, PASSWORD_BCRYPT);
-echo "Senha: ".$senha."<br>";
-echo "Hash: ".$hash."<br>";
-echo "Verificação: ".(password_verify($senha, $hash) ? "OK" : "Falhou");
-?>
